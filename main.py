5307dbb30f69e7b7907de8d38e2d69108818de8c
Abdul Rasheed <AbdulRasheed.ax1000@gmail.com>
2024-12-09 16:13:51 -0600

    Base Model Prepared

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from source.CNNClassifier import logger
 from source.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from source.CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline 
+import ssl
+import urllib.request as request
+
 
 
 STAGE_NAME = "Data Ingestion stage"
+ssl_context = ssl._create_unverified_context()
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
@@ -11,3 +16,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise
+ 
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
